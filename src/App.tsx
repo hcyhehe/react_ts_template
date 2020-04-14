3c180620f8959b4a6a9da70743838483e3dc3e94
Chunying He <chunying.he@ericsson.com>
2020-04-14 18:37:06 +0800

    commit init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './views/home';
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path='/home' component={Home} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
